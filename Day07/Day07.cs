b14fe7a3056c24026a537f8620f2448f6fcd8ec2
madors <madors@microsoft.com>
2020-12-08 12:43:01 -0800

    Optimize Day 07 solution

--- Day07/Day07.cs
+++ Day07/Day07.cs
@@ -11,97 +11,114 @@ namespace Advent2020
         public string FirstStarInputFile { get; } = "input.txt";
         public string SecondStarInputFile { get; } = "input.txt";
 
-        private Dictionary<string, List<ValueTuple<int, string>>> BagGraph;
+        private Dictionary<string, Bag> BagList;
         private const string ShinyGoldBagColor = "shiny gold";
 
         public string SolveFirstStar(StreamReader reader)
         {
             var listOfDefinitions = StreamParsers.GetStreamAsStringList(reader);
-            BuildBagGraph(listOfDefinitions);
+            BuildBagList(listOfDefinitions);
 
-            int bagCount = 0;
-            foreach (var bagColor in BagGraph.Keys)
-            {
-                if (ContainsBag(bagColor, ShinyGoldBagColor))
-                    bagCount++;
-            }
+            var parentBags = new HashSet<string>();
+            GetParentBags(ShinyGoldBagColor, parentBags);
 
             // Includes shiny gold bag, subtract one for answer
-            return (bagCount - 1).ToString();
+            return (parentBags.Count() - 1).ToString();
         }
 
         public string SolveSecondStar(StreamReader reader)
         {
             var listOfDefinitions = StreamParsers.GetStreamAsStringList(reader);
-            BuildBagGraph(listOfDefinitions);
+            BuildBagList(listOfDefinitions);
 
             // Includes shiny gold bag, subtract one for final answer
-            int bagCount = CountBags(ShinyGoldBagColor);
+            int bagCount = CountInnerBags(ShinyGoldBagColor);
 
             return (bagCount - 1).ToString();
         }
 
-        private bool ContainsBag(string startBag, string endBag)
+        private void GetParentBags(string startBag, ISet<string> parentBags)
         {
-            if (startBag == endBag)
-                return true;
-
-            foreach (var subBag in BagGraph[startBag])
+            parentBags.Add(startBag);
+            foreach (var parentBag in BagList[startBag].ParentColors)
             {
-                if (ContainsBag(subBag.Item2, endBag))
-                    return true;
+                GetParentBags(parentBag, parentBags);
             }
-
-            return false;
         }
 
-        private int CountBags(string startBag)
+        private int CountInnerBags(string startBag)
         {
-            if (BagGraph[startBag].Count == 0)
+            if (BagList[startBag].InnerBags.Count == 0)
                 return 1;
 
-            int sumOfSubBags = 1;
-            foreach (var subBag in BagGraph[startBag])
+            int numInnerBags = 1;
+            foreach (var innerBag in BagList[startBag].InnerBags)
             {
-                sumOfSubBags += (subBag.Item1 * CountBags(subBag.Item2));
+                numInnerBags += (innerBag.Item1 * CountInnerBags(innerBag.Item2));
             }
 
-            return sumOfSubBags;
+            return numInnerBags;
         }
 
-        private void BuildBagGraph(List<string> listOfDefinitions)
+        private void BuildBagList(List<string> listOfDefinitions)
         {
-            BagGraph = new Dictionary<string, List<ValueTuple<int, string>>>();
+            BagList = new Dictionary<string, Bag>();
             foreach (var definition in listOfDefinitions)
             {
-                var bagAndSubBags = definition.Split("bags contain").Select(x => x.Trim()).ToList();
-                var subBags = StringParsers.SplitDelimitedStringIntoStringList(bagAndSubBags[1], new char[] { ',' });
+                var bagAndInnerBags = definition.Split("bags contain").Select(x => x.Trim()).ToList();
+                var currentBagColor = bagAndInnerBags[0];
+                var innerBags = StringParsers.SplitDelimitedStringIntoStringList(bagAndInnerBags[1], ",".ToCharArray());
 
-                var listOfRules = new List<ValueTuple<int, string>>();
-                if (bagAndSubBags[1] != "no other bags.")
+                var currentBag = GetOrCreateBag(currentBagColor);
+                if (bagAndInnerBags[1] != "no other bags.")
                 {
-                    foreach (var subBag in subBags)
+                    foreach (var innerBagDescription in innerBags)
                     {
-                        listOfRules.Add(CreateBagNode(subBag));
+                        var innerBagTuple = CreateBagTuple(innerBagDescription);
+                        currentBag.InnerBags.Add(innerBagTuple);
+                        var innerBag = GetOrCreateBag(innerBagTuple.Item2);
+                        innerBag.ParentColors.Add(currentBagColor);
                     }
                 }
-
-                BagGraph.Add(bagAndSubBags[0], listOfRules);
             }
         }
 
-        private ValueTuple<int, string> CreateBagNode(string bagRule)
+        private Bag GetOrCreateBag(string color)
+        {
+            if (BagList.ContainsKey(color))
+                return BagList[color];
+
+            var newBag = new Bag(color);
+            BagList[color] = newBag;
+            return newBag;
+        }
+
+        private ValueTuple<int, string> CreateBagTuple(string bagRule)
         {
             var ruleToReturn = new ValueTuple<int, string>();
             var splitRule = bagRule.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
             ruleToReturn.Item1 = int.Parse(splitRule[0]);
-            var type = "";
+            var color = "";
             for (int i = 1; i < splitRule.Length - 1; ++i)
             {
-                type += " " + splitRule[i].Trim();
+                color += " " + splitRule[i];
             }
-            ruleToReturn.Item2 = type.Trim();
+            ruleToReturn.Item2 = color.Trim();
             return ruleToReturn;
+        }
+
+        private class Bag
+        {
+            public Bag(string color)
+            {
+                Color = color;
+                ParentColors = new HashSet<string>();
+                InnerBags = new List<ValueTuple<int, string>>();
+            }
+
+            public string Color { get; }
+            public ISet<string> ParentColors { get; set; }
+            public List<ValueTuple<int, string>> InnerBags { get; set; }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Advent2020
         static void Main(string[] args)
         {
             // Create solver
-            ISolver solver = new Day08();
+            ISolver solver = new Day07();
 
             try
             {
